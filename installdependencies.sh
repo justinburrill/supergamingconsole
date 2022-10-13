a61560050fa5f4efd1618e5590715f79f12ec90a
Justin Burrill <me@justinburrill.ca>
2022-10-13 12:48:42 -0400

    pong

--- installdependencies.sh
+++ installdependencies.sh
@@ -2,3 +2,5 @@
 # games i wrote are in a different git, run me to install them nicely :)
 cd py && curl https://raw.githubusercontent.com/justinburrill/pysnake/master/snake.py --output snake.py -s
 cd ..
+cd pong && curl https://raw.githubusercontent.com/justinburrill/pypong/main/pong.py --output pong.py -s
+cd ..
