c7b700224fa054b768c1f3e9141f5d5371530342
justinburrill <me@justinburrill.ca>
2022-11-11 13:06:50 -0500

    rm install script

--- installdependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# games i wrote are in a different git, run me to install them nicely :)
-rm -r py
-mkdir py && cd py
-curl https://raw.githubusercontent.com/justinburrill/pysnake/master/snake.py --output snake.py -s
-curl https://raw.githubusercontent.com/justinburrill/pypong/main/pong.py --output pong.py -s
